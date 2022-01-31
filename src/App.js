89f702a89cabe3bb67b9be955bdaf382fb5d47cc
Clayton Knapp <csjknapp@gmail.com>
2022-01-31 12:59:20 -0800

    images and name working

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('AnyTownUSA');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [slogans, setSlogans] = useState(['The city that forced me to quit being an Artist and become a coder', 'Man, the weather sucks here from October-April']);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City 
+        skylineId = {skylineId}
+        waterfrontId = {waterfrontId}
+        castleId = {castleId}
+      />
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
